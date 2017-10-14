f71294fb93aacc3d9898995f56ba88eef88585a5
Momdo Nakamura <xmomdo@gmail.com>
2017-10-14 15:40:20 +0900

    waicサーバ側の更新の取り込み

--- WCAG20/waic_link.js
+++ WCAG20/waic_link.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("body>div:first-child>p:first-child").after("<p>この文書内にあるリンクのうち、「Understanding WCAG 2.0」と「Techniques for WCAG 2.0」へのリンクについては、WAIC の公開する日本語版にリンク先を追加しています。WAIC の日本語訳は、 W3C の公開する英語版より内容が古い可能性がありますのでご注意ください。</p>");
+	$("body>div:first-child>p:first-child").after("<p>この文書内にあるリンクのうち、「Understanding WCAG 2.0」「Techniques for WCAG 2.0」「How to Meet WCAG 2.0」へのリンクについては、WAIC の公開する日本語版にリンク先を追加しています。WAIC の日本語訳は、 W3C の公開する英語版より内容が古い可能性がありますのでご注意ください。</p>");
 	$("a[href*=http]").each(function(){		
 		if( $(this).attr('href').indexOf("UNDERSTANDING-WCAG20")!=-1 ){
 			var url = $(this).attr('href').replace('http:\/\/www.w3.org\/TR\/UNDERSTANDING-WCAG20','http://waic.jp/docs/UNDERSTANDING-WCAG20');
@@ -8,6 +8,11 @@ $(function(){
 		}		
 		if( $(this).attr('href').indexOf("WCAG20-TECHS")!=-1 ){
 			var url = $(this).attr('href').replace('http:\/\/www.w3.org\/TR\/WCAG20-TECHS','http://waic.jp/docs/WCAG-TECHS');
+			var text = $(this).text();
+			$(this).after(" <a href='"+url+"' title='"+text+"の日本語訳'>[日本語訳]</a>");
+		}
+		if( $(this).attr('href').indexOf("quickref")!=-1 ){
+			var url = $(this).attr('href').replace('http:\/\/www.w3.org\/WAI\/WCAG20','http://waic.jp/docs/WCAG20');
 			var text = $(this).text();
 			$(this).after(" <a href='"+url+"' title='"+text+"の日本語訳'>[日本語訳]</a>");
 		}
